4516c643f98651784d5185be1e2675a87f1c4d29
mc007 <asd28071977>
2015-02-20 20:21:38 +0100

    =auto-commit

--- A/ws/workspace/app.js
+++
@@ -1,9 +0,0 @@
-/*
- * This file is provided for custom JavaScript logic that your HTML files might need.
- * Maqetta includes this JavaScript file by default within HTML pages authored in Maqetta.
- */
-require(["dojo/ready"], function(ready){
-     ready(function(){
-     		console.log('	dojo ready!');
-     });
-});
